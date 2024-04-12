ee4246987a2dae9c6e2f4542dcb1c95de83b192d
omar-dulaimi <o.m.dulaimi@gmail.com>
2024-04-12 17:53:40 +0300

    fix getEnumValues helper

--- src/generate-helpers.ts
+++ src/generate-helpers.ts
@@ -2,14 +2,16 @@ import { SourceFile } from 'ts-morph';
 
 export function generateHelpersIndexFile(sourceFile: SourceFile) {
   sourceFile.addStatements(/* ts */ `
-    export function getEnumValues<T>(enumType: T): Array<string> {
+    type EnumLike = Array<unknown> | Record<string, unknown>;
+
+    export function getEnumValues<T extends EnumLike>(enumType: T): Array<string> {
       return [
         ...new Set(
           Object.entries(enumType)
             .filter(([key]) => !~~key)
-            .flatMap((item) => item),
+            .flatMap((item) => item)
         ),
-      ]
-    }
+      ] as Array<string>;
+    }  
   `);
 }
